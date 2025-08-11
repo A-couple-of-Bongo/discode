9074e5ff1a2e0e3c4d3825c19ab2e6b21bbcc22c
Huy-DNA <huydo862003@gmail.com>
2025-08-11 17:02:38 +0700

    feat: fetch daily question link

--- src/commands/daily-question/index.ts
+++ src/commands/daily-question/index.ts
@@ -8,11 +8,30 @@ DiscordClient.installGlobalCommand({
 });
 
 export const dailyQuestion: CommandHandler = async () => {
+  const link = await fetch('https://leetcode.com/graphql/', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      query: `
+        query questionOfTodayV2 {
+          activeDailyCodingChallengeQuestion { link }
+        }
+      `,
+    })
+  }).then((res) => res.json())
+    .then((res) => `https://leetcode.com${(res as any).data.activeDailyCodingChallengeQuestion.link}`);
+
   return {
     type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
     data: {
       flags: InteractionResponseFlags.IS_COMPONENTS_V2,
       components: [
+        {
+          type: MessageComponentTypes.TEXT_DISPLAY,
+          content: `Daily challenge's link: ${link}`,
+        }
       ],
     },
   }
