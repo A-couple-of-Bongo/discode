ad89881e6ff0cb7cfaf2f09ff36c46cf62e79238
Huy-DNA <huydo862003@gmail.com>
2025-08-12 12:32:10 +0700

    fix: server migration data type

--- migrations/20250812041702_servers.sql
+++ migrations/20250812041702_servers.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 CREATE TABLE servers (
-  server_id INT PRIMARY KEY,
-  notified_channel_id INT UNIQUE,
-  notified_role_id INT
+  server_id TEXT PRIMARY KEY,
+  notified_channel_id TEXT UNIQUE,
+  notified_role_id TEXT
 );
 
 -- migrate:down
