fade0c2fce7f18689a16e7ca603b3ee6c2da51a0
Huy-DNA <huydo862003@gmail.com>
2025-08-12 16:54:03 +0700

    feat: help command

---
+++ lib/commands/help/index.ts
@@ -0,0 +1,30 @@
+import { InteractionResponseFlags, InteractionResponseType, MessageComponentTypes } from 'discord-interactions';
+import { CommandHandler } from '..';
+
+export const helpCommand = {
+  name: 'help',
+  description: 'Get usage guidance.',
+};
+
+export const helpHandler: CommandHandler = async () => {
+  return {
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+      components: [
+        {
+          type: MessageComponentTypes.TEXT_DISPLAY,
+          content: `**Discode bot**
+If this is the first time you add the bot to the server you may want to run \`/notify\` to setup the notification.
+------------------------
+**Command**
+- \`daily-question\`: Fetch the daily question from Leetcode.
+- \`notify\`: Setup the notification forum, role and message.
+- \`ping\`: Ping the server.
+------------------------
+`,
+        },
+      ],
+    },
+  }
+}
--- lib/commands/index.ts
+++ lib/commands/index.ts
@@ -1,4 +1,5 @@
 import { dailyQuestionCommand, dailyQuestionHandler } from './daily-question';
+import { helpCommand, helpHandler } from './help';
 import { notifyCommand, notifyHandler } from './notify';
 import { pingCommand, pingHandler } from './ping';
 
@@ -8,10 +9,12 @@ export const commandHandlers: Record<string, CommandHandler> = {
   'ping': pingHandler,
   'daily-question': dailyQuestionHandler,
   'notify': notifyHandler,
+  'help': helpHandler,
 };
 
 export const commands: Record<string, object> = {
   'ping': pingCommand,
   'daily-question': dailyQuestionCommand,
   'notify': notifyCommand,
+  'help': helpCommand,
 }
