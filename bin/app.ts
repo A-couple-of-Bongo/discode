2055eee3972b362d2dd7912f5c408166f477a11a
Huy-DNA <huydo862003@gmail.com>
2025-08-19 18:57:05 +0700

    feat: improve access logging

--- bin/app.ts
+++ bin/app.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import fs from 'fs';
 import express from 'express';
 import nodeCron from 'node-cron';
 import { InteractionResponseType, InteractionType, verifyKeyMiddleware } from 'discord-interactions';
@@ -7,30 +8,48 @@ import morgan from 'morgan';
 import { jobs } from '../lib/cronjobs';
 
 const PORT = process.env.PORT || 3000;
+const LOGFILE_PATH = process.env.LOGFILE_PATH;
 const app = express();
 
-app.use(morgan('tiny'));
 app.use(express.static('public'))
 
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY!), async function(req, res) {
-  const { id, type, data } = req.body;
+app.post('/interactions',
+  morgan(
+    function(tokens, req, res) {
+      if (!req.body) return;
 
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
+      const { id, type, data } = req.body;
+      const timestamp = new Date().toISOString();
+      const userAgent = req.get('User-Agent') || 'Unknown';
 
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-    if (!commandHandlers[name]) {
-      console.error(`Unknown command: ${name}`);
-      return res.status(400).json({ error: 'Unknown command' });
+      const logType = type === InteractionType.APPLICATION_COMMAND
+        ? `Command (name: ${data.name})`
+        : `Non-command (type: ${type})`;
+
+      return `[${timestamp}] Id: ${id} - ${logType} - ${tokens.method?.(req, res)} ${tokens.url?.(req, res)} ${tokens.status?.(req, res)} ${tokens['response-time']?.(req, res)}ms - UA: "${userAgent}"`;
+    },
+    { stream: fs.createWriteStream(LOGFILE_PATH || 'access.log', { flags: 'a' }) },
+  ),
+  verifyKeyMiddleware(process.env.PUBLIC_KEY!),
+  async function(req, res) {
+    const { id, type, data } = req.body;
+
+    if (type === InteractionType.PING) {
+      return res.send({ type: InteractionResponseType.PONG });
     }
-    return res.send(await commandHandlers[name]!(req.body));
-  }
 
-  console.error(`Unknown interaction type: ${type}`);
-  return res.status(400).json({ error: 'Unknown interaction' });
-});
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name } = data;
+      if (!commandHandlers[name]) {
+        console.error(`Unknown command: ${name}`);
+        return res.status(400).json({ error: 'Unknown command' });
+      }
+      return res.send(await commandHandlers[name]!(req.body));
+    }
+
+    console.error(`Unknown interaction type: ${type}`);
+    return res.status(400).json({ error: 'Unknown interaction' });
+  });
 
 app.listen(PORT, () => {
   console.log('Express app starts')
